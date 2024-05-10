ab8be86846169879025747ba020f31a9332e2bdb
enosco <scottrweno@gmail.com>
2024-05-10 17:04:42 -0400

    Added color output, Iterate_Zip now uses array instead of temp file

--- metaname.sh
+++ metaname.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-#TODO:
-    #make sure renaming follows the path of given directory
-    #Stop from nesting folders if folder already exists
-    #Handle case where .zip archive has multiple albums zipped inside
-    #Package renaming into a single function
-    #Figure out bug in zip mode where it creates an empty folder of the selected album
-        #Test on yot club or any agaisnt me album, that seems to provoke it
-    #Another error with wecool.zip, something to do with folder with a folder? figure it out
-        #Freaks out with certain downloads that have osx metadata folders?
-        #mv: cannot move 'wecool.zip' to a subdirectory of itself, '/media/wizard/EVIL JAMS//Jeff Rosenstock - We Cool?.zip'
-            #Found error, cannot use '?' in a filename
-    #Aphex twin album leads to stop, what happens?
-        #Same with me like bees
-    #Found issue with hop along album, have to escape the brackets; how do i do that with input?
-
 set -o errexit   # stop script when an error occurs
 set -o nounset   # stop if an unset variable is accessed
 #set -o xtrace
@@ -38,7 +23,6 @@ Help()
 Rename()
 {
 	if [[ $zip_mode == 1 ]]; then
-
     	new_name="$path/$artist_name - $album_name" #the proper path is already defined in Iterate_Zip
 		mv -nv "$targetdir" "$new_name.zip"	
 
@@ -83,8 +67,6 @@ Iterate_Dir()
 	    
 	done
 
-
-
 	if (( $user_confirm == 1 )); then
 		Rename
 	else
@@ -95,10 +77,7 @@ Iterate_Dir()
 }
 
 #Extract individual songs from archive to grab metadata from
-#TODO
-#Change file list into string or array
-#Find if there is a way to read file without extracting
-Iterate_Zip2()
+Iterate_Zip()
 {
 	path=$(realpath "$targetdir" | sed 's:\(.*/\).*:\1:' )					#Get path of target .zip
 
@@ -141,67 +120,6 @@ Iterate_Zip2()
 		echo
 		exit 1  		
 	fi	
-}
-Iterate_Zip()
-{	
-
-	path=$(realpath "$targetdir" | sed 's:\(.*/\).*:\1:' )					#Get path of target .zip
-	
-	file_list="${path}metaname_temp.txt"									#Create file list of archive's contents in temp dir
-
-	unzip -Z1 "$targetdir" | grep $audio_formats > "$file_list"				#Fill file list			
-	
-	song_count=$(wc -l < "$file_list")
-	
-	for	(( i=1 ; i <= $song_count ; i++ ))
-	do
-
-		current_song=$( sed -n "${i}p" "$file_list" )						#sed command prints the line at index
-		unzip -q "$targetdir" "$current_song" -d "." 						#Grab single song from list
-		
-		printf '\e[0;34m%s\e[0m' "selected audio file: "
-		printf "$current_song\n"
-
-		artist_name=$(exiftool -b -Artist "$current_song" )					#Attempt to pull metadata from song
-		album_name=$(exiftool -b -Album "$current_song" )
-		
-		rm "$current_song"													#Remove song
-		
-		user_confirm=0
-								
-	    printf '\e[0;32m%s\e[0m' "found artist and album name"
-	    read -n 1 -p " -- $album_name by $artist_name -- is this correct (y/n)? : " input
-	    echo
-	    
-	    if [[ "$input" == [Yy] ]]; then
-	    	user_confirm=1
-	        break
-	    else
-	    	printf "trying other audio files...\n\n"
-			continue
-	    fi
-
-	done
-
-	if (( $user_confirm == 1 )); then
-	    #if (( $rev_mode == 1 )); then
-	    #	new_name="$path/$album_name - $artist_name"
-	    #else 
-	    #	new_name="$path/$artist_name - $album_name" #That forward slash creates some formatting weirdness, but it makes the code more readable
-	    #fi
-	    
-        #echo
-	   	#mv -nv "$targetdir" "$new_name.zip"
-        #echo
-	   	Rename
-	else
-		printf '\e[31m%s\e[0m' "unable to find additional audio files. exiting..."
-		echo
-		exit 1  		
-	fi
-
-	rm "$file_list"
-
 }
 
 #						=== MAIN ===
@@ -274,7 +192,7 @@ artist_name="NULL"
 album_name="NULL"
 
 if [[ $zip_mode == 1 ]]; then
-	Iterate_Zip2
+	Iterate_Zip
 else 
 	Iterate_Dir
 fi
